ac2f2da73597d48f8dae6b6c51c12b9c8c15a363
Robin Dönnebrink <doennebrink77@googlemail.com>
2025-08-20 21:42:33 +0200

    Feat: create inital model

---
+++ django/core/models.py
@@ -0,0 +1,50 @@
+"""
+Description: [Add module purpose here]
+
+Author: Robin
+Created: 20.08.2025
+Copyright: © 2025 Robin Dönnebrink
+"""
+
+from django.db import models
+
+
+class ApplicationEntry(models.Model):
+    class Status(models.TextChoices):
+        APPLIED = "applied", "Applied"
+        INTERVIEW1 = "interview1", "Interview 1"
+        INTERVIEW2 = "interview2", "Interview 2"
+        INTERVIEW3 = "interview3", "Interview 3"
+        INTERVIEW4 = "interview4", "Interview 4"
+        INTERVIEW5 = "interview5", "Interview 5"
+        OFFER = "offer", "Offer"
+        REJECTED = "rejected", "Rejected"
+
+    class Stage(models.TextChoices):
+        ACKNOWLEDGMENT = "acknowledgment", "Acknowledgment"
+        INTERVIEW1 = "interview1", "Interview 1"
+        INTERVIEW2 = "interview2", "Interview 2"
+        INTERVIEW3 = "interview3", "Interview 3"
+        INTERVIEW4 = "interview4", "Interview 4"
+        INTERVIEW5 = "interview5", "Interview 5"
+        OFFER = "offer", "Offer"
+        REJECTED = "rejected", "Rejected"
+
+    company = models.CharField(max_length=255)           # Firma
+    application_date = models.DateField()                # Bewerbungsdatum
+    job_title = models.CharField(max_length=255)         # Stellenbezeichnung
+    job_posting = models.TextField(blank=True, null=True)  # Stellenausschreibung
+    status = models.CharField(                           # Stand
+        max_length=20,
+        choices=Status.choices,
+        default=Status.APPLIED,
+    )
+    max_stage = models.CharField(                        # Maximale Stufe
+        max_length=20,
+        choices=Stage.choices,
+        default=Stage.ACKNOWLEDGMENT,
+    )
+    last_update = models.DateField(auto_now=True)        # Last update
+
+    def __str__(self):
+        return f"{self.company} - {self.job_title} ({self.get_status_display()})"
