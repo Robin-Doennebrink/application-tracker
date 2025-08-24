29115f8f816c75a2058c127eb617b016a7eb5d98
Robin Dönnebrink <doennebrink77@googlemail.com>
2025-08-24 21:26:47 +0200

    feat: Open dialog to add new application.

--- frontend/src/app/applications/application-overview/application-overview.component.ts
+++ frontend/src/app/applications/application-overview/application-overview.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import {ApplicationEntry} from "../../models/application-entry.model";
 import {ApplicationsService} from "../../services/application.service";
 import {CommonModule} from "@angular/common";
+import {AddApplicationDialogComponent} from "../add-application-dialog/add-application-dialog.component";
+import {MatDialog} from "@angular/material/dialog";
 
 @Component({
   selector: 'app-application-overview',
@@ -15,12 +17,27 @@ export class ApplicationOverviewComponent {
   loading = true;
   error: string | null = null;
 
-  constructor(private api: ApplicationsService) {}
+  constructor(private api: ApplicationsService, private dialog: MatDialog) {}
 
   ngOnInit(): void {
     this.api.list().subscribe({
       next: (data: ApplicationEntry[]) => { this.entries = data; this.loading = false; },
       error: (err: any) => { this.error = 'Failed to load entries'; this.loading = false; console.error(err); }
+    });
+  }
+
+  addApplication(): void {
+      const dialogRef = this.dialog.open(AddApplicationDialogComponent, {
+      width: '400px',
+      height: '600px',
+      data: {}
+    });
+
+    dialogRef.afterClosed().subscribe((result: any) => {
+      if (result) {
+        console.log('New application:', result);
+        // Add to your application list or send to backend
+      }
     });
   }
 
