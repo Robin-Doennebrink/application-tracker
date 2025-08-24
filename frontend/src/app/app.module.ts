cd7bf68d694537d905761b1a94a73fa9a7c34d60
Robin Dönnebrink <doennebrink77@googlemail.com>
2025-08-24 21:27:30 +0200

    feat: Use google material design.

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppComponent } from './app.component';
 import {RouterOutlet, Routes} from "@angular/router";
 import { RouterModule } from '@angular/router';
 import {ApplicationOverviewComponent} from "./applications/application-overview/application-overview.component";
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 const routes: Routes = [
     {path: '', component: ApplicationOverviewComponent}
@@ -12,7 +14,10 @@ const routes: Routes = [
 
 @NgModule({
     declarations: [AppComponent],
-    imports: [BrowserModule, HttpClientModule, RouterOutlet, RouterModule.forRoot(routes),],
-    bootstrap: [AppComponent]
+    imports: [BrowserModule, HttpClientModule, RouterOutlet, RouterModule.forRoot(routes), BrowserAnimationsModule ],
+    bootstrap: [AppComponent],
+    providers: [
+      provideAnimationsAsync()
+    ]
 })
 export class AppModule {}
