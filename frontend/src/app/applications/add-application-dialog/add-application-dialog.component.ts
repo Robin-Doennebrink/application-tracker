e071fed8aef4802d207c5871b87c39d2a53a7c1e
Robin Dönnebrink <doennebrink77@googlemail.com>
2025-08-25 12:24:50 +0200

    feat: Allow adding new application to track

--- frontend/src/app/applications/add-application-dialog/add-application-dialog.component.ts
+++ frontend/src/app/applications/add-application-dialog/add-application-dialog.component.ts
@@ -5,13 +5,18 @@ import {MatFormFieldModule} from "@angular/material/form-field";
 import {MatInputModule} from "@angular/material/input";
 import { Validators } from '@angular/forms';
 import {MatButtonModule} from "@angular/material/button";
+import {MatDatepickerModule} from "@angular/material/datepicker";
+import {provideNativeDateAdapter} from "@angular/material/core";
 
 @Component({
   selector: 'app-add-application-dialog',
   templateUrl: './add-application-dialog.component.html',
   styleUrls: ['./add-application-dialog.component.scss'],
   standalone: true,
-    imports: [MatFormFieldModule,ReactiveFormsModule,MatDialogModule, MatInputModule, MatButtonModule]
+  imports: [MatFormFieldModule,ReactiveFormsModule,MatDialogModule, MatInputModule, MatButtonModule, MatDatepickerModule],
+  providers: [
+        provideNativeDateAdapter()
+    ]
 })
 export class AddApplicationDialogComponent {
   form: FormGroup;
@@ -23,19 +28,32 @@ export class AddApplicationDialogComponent {
   ) {
     this.form = this.fb.group({
       company: ['', Validators.required],
-      title: ['', Validators.required],
+      job_title: ['', Validators.required],
       link_to_job_advertisement: ['', Validators.required],
-      comment: ['', Validators.required]
+      comment: ['', Validators.required],
+      application_date: [new Date(), Validators.required],
     });
   }
 
   submit() {
     if (this.form.valid) {
-      this.dialogRef.close(this.form.value);
+      const rawResult = this.form.value;
+      const formattedResult = {
+        ...rawResult,
+        application_date: this.formatDate(rawResult.application_date),
+      }
+      this.dialogRef.close(formattedResult);
     }
   }
 
   cancel() {
     this.dialogRef.close();
+  }
+
+  private formatDate(date: Date): string {
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+    return `${year}-${month}-${day}`;
   }
 }
