cb9dc7676cedd02d19f271824626c219bedd61dc
Robin Dönnebrink <doennebrink77@googlemail.com>
2025-09-08 15:10:11 +0200

    feat(application-statistics): add max stage chart and refactor layout structure
    - Introduce bar chart visualization for max stage data in `ApplicationStatisticsComponent`.
    - Separate layouts for status and max stage charts to enhance clarity.
    - Update template with dual bar chart rendering and responsive container styling.

--- frontend/src/app/applications/application-statistics/application-statistics.ts
+++ frontend/src/app/applications/application-statistics/application-statistics.ts
@@ -3,8 +3,8 @@ import { CommonModule } from '@angular/common';
 import { ApplicationsService } from '../../services/application.service';
 import { AggregatedApplicationData } from '../../models/application-entry.model';
 import {StatusLabels} from "../../models/status.enum";
-import * as PlotlyJS from 'plotly.js-dist-min';
 import { PlotlyModule } from 'angular-plotly.js';
+import {StageLabels} from "../../models/stage.enum";
 
 @Component({
   standalone: true,
@@ -14,15 +14,17 @@ import { PlotlyModule } from 'angular-plotly.js';
   styleUrls: ['./application-statistics.scss']
 })
 export class ApplicationStatisticsComponent implements OnInit {
-  plotData: any[] = [];
-  layout: any = {};
+  statusData: any[] = [];
+  maxStageData: any[] = [];
+  layout1: any = {};
+  layout2: any = {};
   config = { responsive: true };
 
   constructor(private applicationsService: ApplicationsService) {}
 
   ngOnInit(): void {
     this.applicationsService.aggregateInterviews().subscribe((res: AggregatedApplicationData) => {
-      this.plotData = [
+      this.statusData = [
         {
           x: res.status_counts.map(item => StatusLabels[item.status]),
           y: res.status_counts.map(item => item.count),
@@ -31,8 +33,27 @@ export class ApplicationStatisticsComponent implements OnInit {
         },
       ];
 
-      this.layout = {
+      this.maxStageData = [
+        {
+          x: res.max_stage_counts.map(item => StageLabels[item.max_stage]),
+          y: res.max_stage_counts.map(item => item.count),
+          type: 'bar',
+          marker: { color: 'steelblue' },
+        }
+      ]
+
+      this.layout1 = {
         title: 'Applications by Status',
+        yaxis: {
+          title: 'Count', tickmode: 'linear',
+          dtick: 1,
+          tickformat: ',d'
+        },
+        xaxis: { title: 'Count' },
+      };
+
+      this.layout2 = {
+        title: 'Applications by Max Stage',
         yaxis: {
           title: 'Count', tickmode: 'linear',
           dtick: 1,
